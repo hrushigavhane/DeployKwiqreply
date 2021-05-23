8d9152e3109cdf502cc612d92ded937502ff3276
gshubham533 <gshubham533@gmail.com>
2021-05-23 17:17:14 +0530

    l

--- waservice/waservice/settings.py
+++ waservice/waservice/settings.py
@@ -85,7 +85,10 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
+            'charset': 'utf8mb4',
+        'use_unicode': True, 
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        
         }
 
     }
